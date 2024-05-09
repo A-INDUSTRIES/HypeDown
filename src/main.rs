99160b46d159bc7ed008ad3356199b03b8e1ed47
A-INDUSTRIES <contact.aindustries@gmail.com>
2024-05-10 01:18:56 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
-use iced::widget::{button, container, text, Container, Row};
+use iced::border::Radius;
+use iced::widget::button::{Status, Style};
+use iced::widget::{button, column, container, row, text, Container, Row};
 use iced::Alignment;
+use iced::Background;
+use iced::Border;
+use iced::Color;
 use iced::Element;
+use iced::Font;
 use iced::Length;
+use iced::Pixels;
+use iced::Renderer;
+use iced::Shadow;
 use iced::Size;
+use iced::Theme;
 use strum::{Display, EnumIter, IntoEnumIterator};
 
 #[derive(Default)]
-struct App {}
+struct App {
+    show_confirm: bool,
+    after_confirm: Message,
+}
 
-#[derive(Debug, EnumIter, Clone, Display)]
+#[derive(Debug, EnumIter, Clone, Display, Default)]
 enum Message {
+    #[strum(to_string = "")]
+    #[default]
     Shutdown,
+    #[strum(to_string = "")]
     Reboot,
+    #[strum(to_string = "󱤛")]
     LogOut,
+    #[strum(to_string = "󱅞")]
     Lock,
-    Sleep,
 }
 
 impl App {
@@ -22,34 +39,66 @@ impl App {
         let mut buttons = vec![];
         for message in Message::iter() {
             buttons.push(Element::new(
-                button(text(message.to_string())).on_press(message),
+                button(text(message.to_string()))
+                    .on_press(message)
+                    .style(|_, status| Style {
+                        background: match status {
+                            Status::Hovered => {
+                                Some(Background::from(Color::from_rgb8(244, 219, 214)))
+                            }
+                            Status::Pressed => {
+                                Some(Background::from(Color::from_rgb8(238, 153, 160)))
+                            }
+                            _ => Some(Background::from(Color::from_rgb8(183, 189, 248))),
+                        },
+                        text_color: Color::from_rgb8(24, 25, 38),
+                        border: Border {
+                            color: Color::TRANSPARENT,
+                            width: 0.1,
+                            radius: Radius::from(200),
+                        },
+                        shadow: Shadow {
+                            color: Color::TRANSPARENT,
+                            ..Default::default()
+                        },
+                    })
+                    .width(Length::from(66))
+                    .height(Length::from(75)),
             ));
         }
+        let confirm: Container<Message, Theme, Renderer> = container(column![
+            text("Are you sure?"),
+            row![button("Yes"), button("Cancel")]
+        ]);
         let line = Row::from_vec(buttons)
             .align_items(Alignment::Center)
             .spacing(5);
-        container(line)
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .center()
+        if self.show_confirm {
+            container(column![container(line), confirm])
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .center()
+        } else {
+            container(line)
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .center()
+        }
     }
 
     fn update(&mut self, message: Message) {
         match message {
             Message::Shutdown => {
-                println!("/!\\TODO")
+                self.show_confirm = true;
             }
             Message::Reboot => {
-                println!("/!\\TODO")
+                self.show_confirm = true;
             }
             Message::LogOut => {
-                println!("/!\\TODO")
+                self.show_confirm = true;
             }
             Message::Lock => {
-                println!("/!\\TODO")
-            }
-            Message::Sleep => {
-                println!("/!\\TODO")
+                self.show_confirm = true;
             }
         }
     }
@@ -59,11 +108,13 @@ fn main() -> Result<(), iced::Error> {
     iced::program("HyperDown", App::update, App::view)
         .settings(iced::Settings {
             window: iced::window::Settings {
-                size: Size::new(400f32, 50f32),
-                max_size: Some(Size::new(400f32, 50f32)),
+                size: Size::new(300f32, 95f32),
                 ..Default::default()
             },
+            default_font: Font::with_name("Hack Nerd Font"),
+            default_text_size: Pixels::from(50),
             ..Default::default()
         })
+        .theme(|_| Theme::CatppuccinMacchiato)
         .run()
 }
